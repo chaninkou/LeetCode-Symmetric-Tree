e3365853b698734cc81310850c3ecda4c5b1b435
chaninkou <michaelkoulove@yahoo.com>
2020-01-20 18:35:21 -0800

    Great review on checking if a tree is symmetric recursive.

--- src/tree/CheckIfSymmetricTreeFunction.java
+++ src/tree/CheckIfSymmetricTreeFunction.java
@@ -2,13 +2,16 @@ package tree;
 
 public class CheckIfSymmetricTreeFunction {
     public boolean isSymmetric(TreeNode root) {
-        // If root is null, return false or check left subtree and right subtree if root is not null
+    	if(root == null){
+    		return true;
+    	}
+        // check left subtree and right subtree to see if its symmetric
         return root == null || checkingLeftRight(root.left, root.right);
     }
     
     // Checking if left subtree node are same as right subtree node
     private boolean checkingLeftRight(TreeNode leftSub, TreeNode rightSub){
-        // If there is only root, return true
+        // If reach till end where both children is null, return true
         if(leftSub == null && rightSub == null){
             return true;
         }
@@ -18,8 +21,7 @@ public class CheckIfSymmetricTreeFunction {
             return false;
         }
         
+        // Check the value if they are equal, then check their child patterns
         return leftSub.val == rightSub.val && checkingLeftRight(leftSub.left, rightSub.right) && checkingLeftRight(leftSub.right, rightSub.left);
-
-    
     }
 }
